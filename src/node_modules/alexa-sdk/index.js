a566ace04eddb2d134652729364503dcc6f46d72
Jeff Blankenburg <github@jeffblankenburg.com>
2017-02-01 09:28:15 -0500

    Removed redundancies

--- src/node_modules/alexa-sdk/index.js
+++
@@ -1,5 +0,0 @@
-var AlexaLambdaHandler = require('./lib/alexa');
-
-module.exports.handler = AlexaLambdaHandler.LambdaHandler;
-module.exports.CreateStateHandler = AlexaLambdaHandler.CreateStateHandler;
-module.exports.StateString = AlexaLambdaHandler.StateString;
